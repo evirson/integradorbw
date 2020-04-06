d319fb8c836ce7f429cb89f364d7549e4508ef5e
evirson <evirson@vetorsistemas.com.br>
2020-04-05 22:00:51 -0300

    adicionadas as variacoes

--- src/main/java/br/com/vetorsistemas/integradorbw/IntegraBwCenterApplication.java
+++ src/main/java/br/com/vetorsistemas/integradorbw/IntegraBwCenterApplication.java
@@ -1,13 +1,18 @@
 package br.com.vetorsistemas.integradorbw;
 
+import javax.xml.bind.JAXBException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IntegraBwCenterApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JAXBException {
 		SpringApplication.run(IntegraBwCenterApplication.class, args);
+		
+		
+	//	Cliente.buscaDados();
 	}
 
 }
